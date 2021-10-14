c144b1ee92be9ed4579c6c28dcb60ba8638d8893
Raffes <rafaelaperes215@gmail.com>
2021-10-14 16:58:43 -0300

    Adicionado pilha | LIFO ao sistema

--- src/one/digital/innovation/pilha/Main.java
+++ src/one/digital/innovation/pilha/Main.java
@@ -3,27 +3,27 @@ package one.digital.innovation.pilha;
 public class Main {
 
     public static void main(String[] args) {
-        Pilha filo = new Pilha();
+        Pilha lifo = new Pilha();
 
         // Adicionar nós a pilha
-        filo.push(new No(1));
-        filo.push(new No(2));
-        filo.push(new No(3));
-        filo.push(new No(4));
-        filo.push(new No(5));
-        filo.push(new No(6));
+        lifo.push(new No(1));
+        lifo.push(new No(2));
+        lifo.push(new No(3));
+        lifo.push(new No(4));
+        lifo.push(new No(5));
+        lifo.push(new No(6));
 
         // Mostra todos os nos
-        System.out.println(filo);
+        System.out.println(lifo);
 
         // fazer o pop | retira o último da pilha
-        System.out.println(filo.pop());
+        System.out.println(lifo.pop());
 
         // mostrando a pilha sem o dado retirado
-        System.out.println(filo);
+        System.out.println(lifo);
 
         // fazer o top | mostra o último da pilha
-        System.out.println(filo.top());
+        System.out.println(lifo.top());
 
         //System.out.println(filo);
 
